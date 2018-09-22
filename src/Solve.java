f30924f0b2303b71d96e268d4055e7917fe1d449
Helle van den Broek <hvbroek@stud.ntnu.no>
2018-09-22 22:40:28 +0200

    Changes to Astar

--- src/Solve.java
+++ src/Solve.java
@@ -18,18 +18,21 @@ public class Solve {
     }
 
     public void aStar(){
+
+        //open set
         List<Node> closedSet = new ArrayList<Node>();
         List<Node> openSet = new ArrayList<Node>();
-
         while (openSet.size() != 0) {
             for (int i = 0; i < openSet.size(); i++) {
                 if (openSet.get(i).getNodecost() < current.getNodecost()) {
                     current = openSet.get(i);
                 }
             }
-
             openSet.remove(current);
             closedSet.add(current);
+
+
+
 
             ArrayList<Node> nb = getNeighbors(current);
 
@@ -47,7 +50,6 @@ public class Solve {
                 this.prev = current;
             }
         }
-
     }
 
     public ArrayList<Node> getNeighbors(Node n){
@@ -57,7 +59,6 @@ public class Solve {
         for (int i = 0; i < nodes.size(); i++){
             int ix = nodes.get(i).getX();
             int iy = nodes.get(i).getY();
-
             if (((x == ix & y == iy+1) | (x== ix && y == iy-1)) | ((y == iy & x == ix +1) | (y == iy & x == ix-1))){
                 neighbors.add(nodes.get(i));
             }
